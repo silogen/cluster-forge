24556702fe57a391e52db27287976f7cc9dd2d07
woojae-siloai <woojae.lim@silo.ai>
2024-11-14 15:51:49 +0200

    add-external-secrets: edit cmd/utils/templates/deploy.sh

--- cmd/utils/templates/deploy.sh
+++ cmd/utils/templates/deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 kubectl apply -f crossplane_base.yaml
-kubectl wait --for=condition=available --timeout=600s deployments --all --all-namespaces
+kubectl wait --for=condition=available --timeout=600s deployments --all -n crossplane-system
 DIRECTORY=$PWD
 
+# Loop through all YAML files in the directory that start with crd
+for file in "$DIRECTORY"/crd*.yaml; do
+  if [ -f "$file" ]; then
+    echo "Applying $file"
+    kubectl apply -f "$file"
+  else
+    echo "No files matching crd*.yaml found in $DIRECTORY"
+  fi
+done
 # Loop through all YAML files in the directory that start with cm_
 for file in "$DIRECTORY"/cm*.yaml; do
   if [ -f "$file" ]; then
@@ -13,13 +22,14 @@ for file in "$DIRECTORY"/cm*.yaml; do
     echo "No files matching cm*.yaml found in $DIRECTORY"
   fi
 done
+
 kubectl apply -f crossplane.yaml
 sleep 20
 kubectl apply -f function-templates.yaml
 kubectl apply -f crossplane_provider.yaml
 kubectl apply -f composition.yaml
 kubectl delete pods --all -n crossplane-system
-kubectl wait --for=condition=Ready --timeout=600s pods --all --all-namespaces
+kubectl wait --for=condition=Ready --timeout=600s pods --all -n crossplane-system
 kubectl apply -f claim.yaml
 helm repo add komodorio https://helm-charts.komodor.io \
   && helm repo update komodorio \
@@ -29,3 +39,4 @@ kubectl wait --for=condition=Ready --timeout=600s pods --all -n default
 
 echo see status with:
 echo kubectl port-forward svc/komoplane 8090:8090
+
