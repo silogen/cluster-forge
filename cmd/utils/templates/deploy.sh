4654d4ad1ebe4b5b6539b2ff5bf8afc2760cf92e
Mika Ranta <Mika.Ranta@amd.com>
2025-02-14 08:58:51 +0200

    Update Excternal-secrets to 0.14.2 and tweak deploy.sh

--- cmd/utils/templates/deploy.sh
+++ cmd/utils/templates/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 kubectl create ns argocd
 kubectl apply -n argocd -f argocd.yaml
 kubectl apply -f gitea.yaml
-sleep 60
+kubectl rollout status deploy/gitea -n cf-gitea
+kubectl rollout status deploy/argocd-applicationset-controller -n argocd
+kubectl rollout status deploy/argocd-redis -n argocd
+kubectl rollout status deploy/argocd-repo-server -n argocd
+kubectl rollout status statefulset/argocd-application-controller -n argocd
 kubectl apply -f argoapp.yaml
