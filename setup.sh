46881fd31b0830f11b522b33354d5e592e91a796
Andy Allred <andy@punasusi.com>
2025-02-20 12:15:23 +0200

    missed one typo

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ cd clusterforge-core-1.0.8
 sudo setup-forge.sh
 INFO=$(gum style --padding "1 4" --border double --border-foreground 57 'Kubernetes has been installed and configured' 'Now ClusterForge will install the stack to enable running workloads')
 gum join --align center --vertical $INFO
-cd 1.0.6-core
+cd 1.0.8-core
 KUBECONFIG=/etc/rancher/rke2/rke2.yaml deploy.sh
