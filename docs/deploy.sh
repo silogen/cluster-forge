4b3985f22dd89002677bf3539a3936114ae19f7d
Andy Allred <andy@punasusi.com>
2025-02-21 09:02:43 +0200

    Consolidate deploy scripts

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,9 +1,3 @@
-wget https://github.com/silogen/cluster-forge/releases/download/1.0.8-core/release-core-1.0.8.tar.gz
-tar xfz release-core-1.0.8.tar.gz
-sudo bash setup-forge.sh
-cd clusterforge-core-1.0.8
-INFO=$(gum style --padding "1 4" --border double --border-foreground 57 'Kubernetes has been installed and configured' 'Now ClusterForge will install the stack to enable running workloads')
-gum join --align center --vertical $INFO
-cd 1.0.8-core
-KUBECONFIG=/etc/rancher/rke2/rke2.yaml && bash deploy.sh
-
+wget https://github.com/silogen/cluster-forge/releases/download/deploy/deploy-release.tar.gz
+tar xfz deploy-release.tar.gz
+bash setup-forge.sh
--- setup.sh
+++
@@ -1,8 +0,0 @@
-wget https://github.com/silogen/cluster-forge/releases/download/1.0.8-core/release-core-1.0.8.tar.gz
-tar xfz release-core-1.0.8.tar.gz
-sudo bash setup-forge.sh
-cd clusterforge-core-1.0.8
-gum log --structured --level info  'Kubernetes has been installed and configured'
-gum log --structured --level info  'Now ClusterForge will install the stack to enable running workloads'
-cd 1.0.8-core
-KUBECONFIG=/etc/rancher/rke2/rke2.yaml && bash deploy.sh
