1282c2490159dfa138b2062ea41e822f84c12695
woojae-siloai <woojae.lim@silo.ai>
2025-03-04 11:58:54 +0200

    add ingress.yaml and update setup-forge.sh

--- setup-cluster/setup-forge.sh
+++ setup-cluster/setup-forge.sh
@@ -276,6 +276,8 @@ main() {
         mkdir -p $HOME/.kube
         sudo sed "s/127\.0\.0\.1/$MAIN_IP/g" /etc/rancher/rke2/rke2.yaml | tee $HOME/.kube/config
         chmod 600 $HOME/.kube/config
+				sudo sed -i "s/10\.7\.125\.221/$MAIN_IP/g" ingress/ingress.yaml
+				KUBECONFIG=/etc/rancher/rke2/rke2.yaml kubectl apply -f ingress/ingress.yaml
         wait_for_api_server 
         KUBECONFIG=$HOME/.kube/config && bash deploy.sh
         gum log --structured --level info 'ClusterForge installed'
