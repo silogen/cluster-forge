88413e2216a91ef4e0babf84fe4d45527bf37a54
Marc Dillon <98823347+brownzebra@users.noreply.github.com>
2025-03-20 14:46:57 +0200

    Update setup-forge.sh
    correct filename for proxy set

--- setup-cluster/setup-forge.sh
+++ setup-cluster/setup-forge.sh
@@ -285,8 +285,8 @@ generate_longhorn_disk_string() {
 update_proxy() {
 	# Variables
 	source_file="proxy.txt"        
-	destination_dir="/etc/default"
-	destination_filename="rke2-server"
+	destination_dir="/usr/local/lib/systemd/system/rke2-server"
+	destination_filename="rke2-server.env"
 
 # Check if the file exists in the current directory
 if [[ -f "$source_file" ]]; then
