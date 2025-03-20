044a7e9a3f2e92b68ad657bcbebb02fcb970f703
woojae-siloai <woojae.lim@silo.ai>
2025-03-20 11:06:38 +0200

    small fix caster

--- cmd/caster/caster.go
+++ cmd/caster/caster.go
@@ -141,8 +141,9 @@ func CastTool(filesDir, imageName string, publishImage bool, stackName string, g
 	utils.ReplaceStringInFile("stacks/latest/argoapp.yaml", "GITOPS_URL", gitops.Url)
 	utils.ReplaceStringInFile("stacks/latest/argoapp.yaml", "GITOPS_BRANCH", gitops.Branch)
 	utils.ReplaceStringInFile("stacks/latest/argoapp.yaml", "GITOPS_PATH_PREFIX", gitops.PathPrefix)
-	if persistentGitea {
+	if gitea.Persistent {
 		utils.CopyFile("cmd/utils/templates/gitea_pvc.yaml", "stacks/latest/gitea.yaml")
+	}
 	if argocdui {
 		utils.CopyFile("cmd/utils/templates/argocd_full.yaml", "stacks/latest/argocd.yaml")
   } else {
