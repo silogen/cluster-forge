885b9c3001087f4458d6712e8b086d212a08a526
Alexander Medvedev <alexander.medvedev@amd.com>
2025-11-14 12:03:43 +0000

    fix: pin --kube-version for helm template

--- scripts/bootstrap_dev.sh
+++ scripts/bootstrap_dev.sh
@@ -2,6 +2,8 @@
 
 DOMAIN="${1:-}"
 VALUES_FILE="${2:-values_dev.yaml}"
+KUBE_VERSION=1.33
+
 if [ -z "$DOMAIN" ]; then
     echo "ERROR: Domain argument is required"
     echo "Usage: $0 <domain> [values_file]"
@@ -13,7 +15,7 @@ kubectl create ns argocd
 kubectl create ns cf-openbao
 
 # ArgoCD bootstrap
-helm template --release-name argocd ../sources/argocd/8.3.0 --namespace argocd | kubectl apply -f -
+helm template --release-name argocd ../sources/argocd/8.3.0 --namespace argocd --kube-version=${KUBE_VERSION} | kubectl apply -f -
 kubectl rollout status statefulset/argocd-application-controller -n argocd
 kubectl rollout status deploy/argocd-applicationset-controller -n argocd
 kubectl rollout status deploy/argocd-redis -n argocd
@@ -21,13 +23,13 @@ kubectl rollout status deploy/argocd-repo-server -n argocd
 
 # OpenBao bootstrap
 helm template --release-name openbao ../sources/openbao/0.18.2 -f ../sources/openbao/values_cf.yaml \
-  --namespace cf-openbao | kubectl apply -f -
+  --namespace cf-openbao --kube-version=${KUBE_VERSION} | kubectl apply -f -
 kubectl wait --for=jsonpath='{.status.phase}'=Running pod/openbao-0 -n cf-openbao --timeout=300s
-helm template --release-name openbao-init ./init-openbao-job --set domain="$DOMAIN" | kubectl apply -f -
+helm template --release-name openbao-init ./init-openbao-job --set domain="$DOMAIN" --kube-version=${KUBE_VERSION} | kubectl apply -f -
 if ! kubectl wait --for=condition=complete --timeout=60s job/openbao-init-job -n cf-openbao; then
   echo "ERROR: Job openbao-init-job failed to complete or timed out!"
   exit 1
 fi
 
 # Create ArgoCD cluster-forge app
-helm template ../root -f ../root/${VALUES_FILE} | kubectl apply -f -
+helm template ../root -f ../root/${VALUES_FILE} --kube-version=${KUBE_VERSION} | kubectl apply -f -
