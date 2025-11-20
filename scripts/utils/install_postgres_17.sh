3e057e94aa5e88938fe58f819f43c98d9fb8c7fa
Daniel Vaskivaara <daniel.vaskivaara@amd.com>
2025-11-20 14:51:21 +0200

    docs: refine docs with streamlined approach since message queues are expected to be empty or not have an impact

--- scripts/utils/install_postgres_17.sh
+++ scripts/utils/install_postgres_17.sh
@@ -7,4 +7,4 @@ sudo apt-key add ACCC4CF8.asc
 echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 
 sudo apt-get update
-sudo apt-get install postgresql-17 postgresql-client-17
+sudo apt-get install -y postgresql-17 postgresql-client-17
