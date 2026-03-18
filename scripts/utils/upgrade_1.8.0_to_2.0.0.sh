8636dd3a749b870f52aab4e34265653c057129d9
Daniel Vaskivaara <daniel.vaskivaara@amd.com>
2026-03-18 10:59:42 +0200

    feat: revise upgrade script to include cleanup of aim sources

--- scripts/utils/upgrade_1.8.0_to_2.0.0.sh
+++ scripts/utils/upgrade_1.8.0_to_2.0.0.sh
@@ -12,23 +12,29 @@ if [[ "$CONFIRM" != "yes" ]]; then
   exit 1
 fi
 
-ARGO_INITIAL_ADMIN=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d && echo)
+# disable auto-sync for cluster-forge
+argocd app set cluster-forge --sync-policy none --source-position 1
 
+ARGO_INITIAL_ADMIN=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d && echo)
 ARGO_POD=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[0].metadata.name}')
 kubectl exec -n argocd "$ARGO_POD" -- sh -c "
-  argocd login localhost:8080 --username admin --password '${ARGO_INITIAL_ADMIN}' --plaintext &&
-  argocd app set cluster-forge --sync-policy none --source-position 1 &&
-  argocd app set airm --sync-policy none --source-position 1 &&
-  argocd app delete airm --cascade=true
-  argocd app delete kaiwo --cascade=true
-  argocd app delete kaiwo-crds --cascade=true
-  argocd app delete kaiwo-config --cascade=true
+  argocd login localhost:8080 --username admin --password '${ARGO_INITIAL_ADMIN}' --plaintext &&  
+  for app in aim-cluster-model-source kaiwo kaiwo-crds kaiwo-config airm; do
+    argocd app get \$app &>/dev/null && \
+    argocd app set \$app --sync-policy none --source-position 1 && \
+    argocd app delete \$app --cascade=true || true
+  done
 "
+
+# wait for the applications to be deleted before proceeding
+echo "Waiting for ArgoCD applications to be deleted..."
+kubectl wait applications.argoproj.io -n argocd --for=delete --timeout=300s \
+  aim-cluster-model-source airm kaiwo kaiwo-crds kaiwo-config
 
 kubectl delete aimclustermodel.aim.silogen.ai --all -A
 kubectl delete aimclustermodelsource.aim.silogen.ai --all -A
 kubectl delete aimclusterservicetemplates.aim.silogen.ai --all -A
 
 # manually delete AIRM secrets that will be recreated by the new app
-kubectl delete secret/airm-tls-secret -n airm --ignore-not-found=false
-kubectl delete secret/airm-rabbitmq-common-vhost-user -n airm --ignore-not-found=false
+kubectl delete secret/airm-tls-secret -n airm --ignore-not-found=true
+kubectl delete secret/airm-rabbitmq-common-vhost-user -n airm --ignore-not-found=true
